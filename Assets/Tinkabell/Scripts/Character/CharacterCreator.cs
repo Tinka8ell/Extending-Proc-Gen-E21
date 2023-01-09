a8bf9d8fc5bd4aef167d1badd825598d3d5938e2
Mark Ingamells <mark.ingamells@gmail.com>
2023-01-09 18:56:19 +0000

    Normalisation

--- Assets/Tinkabell/Scripts/Character/CharacterCreator.cs
+++ Assets/Tinkabell/Scripts/Character/CharacterCreator.cs
@@ -27,6 +27,7 @@ public class CharacterCreator : MonoBehaviour
     private List<string> femaleHairTypes = new List<string>();
 
     void Awake(){
+        Debug.Log("Avatar Awake");
         maleHairTypes.Add("None");
         maleHairTypes.Add("MaleHair1");
         maleHairTypes.Add("MaleHair2");
@@ -39,23 +40,21 @@ public class CharacterCreator : MonoBehaviour
     }
 
     void OnEnable(){
+        Debug.Log("Avatar OnEnable");
         avatar.CharacterUpdated.AddListener(Updated);
-        /*
         heightSlider.onValueChanged.AddListener(ChangeHeight);
-        bellySlider.onValueChanged.AddListener(ChangeBelly);
+        // bellySlider.onValueChanged.AddListener(ChangeBelly);
         skinTones.OnColorPicked.AddListener(ChangeSkinColour);
         hairColours.OnColorPicked.AddListener(ChangeHairColour);
-        */
     }
 
     void OnDisable(){
+        Debug.Log("Avatar OnDisable");
         avatar.CharacterUpdated.RemoveListener(Updated);
-        /*
         heightSlider.onValueChanged.RemoveListener(ChangeHeight);
-        bellySlider.onValueChanged.RemoveListener(ChangeBelly);
+        // bellySlider.onValueChanged.RemoveListener(ChangeBelly);
         skinTones.OnColorPicked.RemoveListener(ChangeSkinColour);
         hairColours.OnColorPicked.RemoveListener(ChangeHairColour);
-        */
     }
 
     void Updated(UMAData dqata){
@@ -64,13 +63,11 @@ public class CharacterCreator : MonoBehaviour
             hairTypes = maleHairTypes;
         else 
             hairTypes = femaleHairTypes;
-        /*
         heightSlider.value = dna["height"].Get();
         bellySlider.value = dna["belly"].Get();
         string hairString = avatar.GetWardrobeItemName("Hair");
         hairType = hairTypes.FindIndex(s => s == hairString);
         hairType = Mathf.Clamp(hairType, 0, hairTypes.Count - 1);
-        */
     }
 
     public void SwitchGender(bool male){
@@ -92,6 +89,7 @@ public class CharacterCreator : MonoBehaviour
     }
 
     public void ChangeSkinColour(Color colour){
+        Debug.Log("Avatar ChangeSkinColour: " + colour);
         avatar.SetColor("Skin", colour);
         avatar.UpdateColors(true);
     }
@@ -110,6 +108,7 @@ public class CharacterCreator : MonoBehaviour
     }
 
     public void ChangeHairColour(Color colour){
+        Debug.Log("Avatar ChangeHairColour: " + colour);
         avatar.SetColor("Hair", colour);
         avatar.UpdateColors(true);
     }
